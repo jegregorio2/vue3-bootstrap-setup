910d9e63a1607913d018722fae6a0bc9dcf157eb
Juan <juan.e.gregorio@gmail.com>
2021-05-14 00:20:47 -0700

    Initial commit

---
+++ src/components/NameComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>name</td>
+          <td>title</td>
+        </tr>
+        <tr>
+          <td>name</td>
+          <td>title</td>
+        </tr>
+        <tr>
+          <td>name</td>
+          <td>title</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmployeeTable',
+  }
+</script>
+
+<style scoped>
+  
+</style>
+
